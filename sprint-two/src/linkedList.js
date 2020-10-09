ed71ecb91e5924b4eecafd3285ac4528fc1f3a17
AmeliaYeoh <ameyjm@gmail.com>
2020-10-08 18:35:38 -0700

    Implemented linkedList methods: addToTail and removeHead

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    this.tail = Node(value);
+    if (this.head === null) {
+      this.head = Node(value);
+    } else if (this.head.next === null) {
+      this.head.next = this.tail;
+    }
+
   };
 
   list.removeHead = function() {
+    var removedHead = this.head;
+    var after = this.head.next;
+    delete this.head;
+    this.head = after;
+    return removedHead.value;
   };
 
   list.contains = function(target) {
+    // var result = false;
+    // iterate over the list values
+    // if current values matches target
+    // update result to true
+
+    // return result
   };
 
   return list;
@@ -26,4 +44,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addToTail - O(1)
+ removeHead - O(1)
+ contains - O(n)
  */
