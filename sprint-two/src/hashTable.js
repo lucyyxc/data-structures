fb10012869683ab873d406bf04a1c4746880b500
lucyyxc <xilucychen@gmail.com>
2020-10-10 15:11:28 -0700

    completed Hash Tables

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,41 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (bucket && k !== bucket[0][0]) {
+    bucket.push([k, v]);
+  } else {
+    this._storage.set(index, [[k, v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(index);
+  if (!bucket) {
+    return undefined;
+  }
+  for (var i = 0; i < bucket.length; i++) {
+    if (k === bucket[i][0]) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each(function(element, ind, storageArr) {
+    if (ind === index) {
+      storageArr.splice(index, 1);
+    }
+  });
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+all constant O(1)
  */
 
 
