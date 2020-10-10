f0c03fd5802b997060e1bdd0c729efb6c128fd37
AmeliaYeoh <ameyjm@gmail.com>
2020-10-09 21:09:45 -0700

    Implemented tree methods

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +10,29 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      if (this.children[i].contains(target) === true) {
+        return true;
+      }
+    }
+  }
+  return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild - O(n)
+ contains - O(n)
+
  */
+
